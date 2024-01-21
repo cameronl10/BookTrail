097084db0c380696ea346148daf74a632c7af94a
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-01-21 02:09:35 -0800

    new backend

--- my-app/src/app/api/findbook/route.ts
+++ my-app/src/app/api/findbook/route.ts
@@ -1,10 +1,8 @@
-import express, { Router, Request, Response } from "express";
 import { URL } from "url";
 import { OpenLibraryResponse } from "./search_types";
-import { mongo_client_config, uri } from "./mongodb";
+import { mongo_client_config, uri } from "../mongodb";
 import { MongoClient } from "mongodb";
-
-const router: Router = express.Router();
+import { NextApiRequest, NextApiResponse } from "next";
 
 const log_search = async (query_string: string, sid: string) => {
     const client = new MongoClient(uri(), mongo_client_config)
@@ -22,17 +20,23 @@ const log_search = async (query_string: string, sid: string) => {
     }
 }
 
-router.get("/search", async (req: Request, res: Response) => {
+export async function GET(req: Request){
+    const {searchParams} = new URL(req.url);
+
     //input sanitization
-    const query_string = req.query.q;
-    const sid = req.query.sid;
+    const query_string = searchParams.get("q");
+    const sid = searchParams.get("sid");
 
     if(!query_string || typeof query_string != 'string')
-        return res.status(500).send(`Query string must be passed in with query param 'q'.
+        return new Response(`Query string must be passed in with query param 'q'.
             Query string must also not be an array.
-            Query string passed in is ${query_string}`)
+            Query string passed in is ${query_string}`, {
+                status: 500
+            })
     if(!sid || typeof sid != "string")
-        return res.status(500).send(`Sid not supplied, or not a string. Provided sid: \"${sid}\"`)
+        return new Response(`Sid not supplied, or not a string. Provided sid: \"${sid}\"`, {
+            status: 500
+        })
     
     
     // log a search with mongodb
@@ -51,11 +55,15 @@ router.get("/search", async (req: Request, res: Response) => {
     url.searchParams.set("limit", "50")
 
     for (let page = 1; matches.length < 1; page++) {
-        if (page > 3) return res.status(500).json({ error: "Cannot Dewey's for this Query" })
+        if (page > 3) return new Response("Cannot Dewey's for this Query", {
+            status: 500
+        })
         // send request
         url.searchParams.set("page", page.toString())
         const response = await fetch(url.toString())
-        if (response.status !== 200) return res.status(500).send(response.statusText)
+        if (response.status !== 200) return new Response(response.statusText, {
+            status: 500
+        })
         // process and add data
         const data: OpenLibraryResponse = await response.json()
         const valid_docs = data.docs.filter(doc => doc.ddc !== undefined && doc.ddc.length > 0 && !isNaN(parseFloat(doc.ddc[0])))
@@ -63,13 +71,11 @@ router.get("/search", async (req: Request, res: Response) => {
             dewey_decimal: parseFloat(match.ddc![0]),
             title: match.title,
             description: "description", // TODO sort this out
-            shelf_id: Math.ceil(Math.round(parseFloat(match.ddc![0])) / (1000 / 30))
+            shelf_id: Math.ceil(Math.round(parseFloat(match.ddc![0])) / (1000 / 168))
         } as RetMatch)))
     }
 
     await mongodb_log_task; // make sure that mongo is done
 
-    return res.json({ match_count: matches.length, matches })
-})
-
-export default router;
+    return Response.json({ match_count: matches.length, matches })
+}
