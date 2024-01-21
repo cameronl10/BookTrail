1e3164bc02e2d5d199eef0b8e71d4de170760ed2
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-01-20 20:16:45 -0800

    login button

--- my-app/src/components/loginButton.tsx
+++ my-app/src/components/loginButton.tsx
@@ -1,8 +1,21 @@
 'use client';
+
+import { useState } from "react";
+
 export default function LoginButton() {
-    return (
-        <a href="/api/auth/login" className="mr-5 font-body text-primary-ubc font-bold text-sm">
-            Login / Sign Up
-        </a>
-    )
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    
+    if(isAuthenticated) {
+        return (
+            <a href="/api/auth/logout" className="mr-5 font-body text-primary-ubc font-bold text-sm">
+                Logout
+            </a>
+        )
+    } else {
+        return (
+            <a href="/api/auth/login" className="mr-5 font-body text-primary-ubc font-bold text-sm">
+                Login / Sign Up
+            </a>
+        )
+    }
 }
