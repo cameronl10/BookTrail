a056e2619269a281b86770dc18870103c8bcd94e
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-01-20 20:07:28 -0800

    Auth

---
+++ my-app/src/app/components/loginButton.tsx
@@ -0,0 +1,8 @@
+'use client';
+export default function LoginButton() {
+    return (
+        <a href="/api/auth/login" className="mr-5 font-body text-primary-ubc font-bold text-sm">
+            Login / Sign Up
+        </a>
+    )
+}
--- my-app/src/app/components/navBar.tsx
+++ my-app/src/app/components/navBar.tsx
@@ -1,9 +1,11 @@
+import LoginButton from "./loginButton";
+
 export default function navBar() {
     return (
     <div>
         <div className="w-screen flex justify-between content-center items-center">
             <img className="ml-3 w-32" src='logo.png' alt="../logo.png"/>
-            <h2 className="mr-5 font-body text-primary-ubc font-bold text-sm">Login / Sign Up</h2>
+            <LoginButton />
         </div>
     </div>
     )
