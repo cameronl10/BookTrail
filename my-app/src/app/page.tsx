900678d3167e9dce49dd29bed7c90d5e1a0ac3da
Jay Park <jaybinpark@gmail.com>
2024-01-20 17:34:39 -0800

    new root

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    
+    <div className="">
+      <div className="text-s">Hello World</div>
+      <div className="text-xl">Hello World</div>
     </div>
   );
 }
