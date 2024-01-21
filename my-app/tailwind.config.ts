7a068a2d10680820af0701c273db4ff487531406
Jay Park <jaybinpark@gmail.com>
2024-01-20 21:01:56 -0800

    big changesgit add .

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -7,12 +7,6 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'primary-ubc': '#002145',
-      'search-primary' : '#222830',
-      'search-bar' : "#363A43",
-      'search-text' : "#A1A4AB"
-    },
     fontFamily: {
       'body': ['"Open Sans"'],
       'heading': ['"Poppins"', 'sans-serif']
@@ -23,6 +17,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'primary-ubc': '#002145',
+        'search-primary' : '#222830',
+        'search-bar' : "#363A43",
+        'search-text' : "#A1A4AB",
+        'search-bold' : "#2c2c2E",
+        'search-highlight' : "#00A7E1",
+      }
+
     },
   },
   plugins: [],
