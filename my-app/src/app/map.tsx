2e974e404e0ad682912fa831bf234e706ab18bda
cameron lee <cameron.lee@smus.ca>
2024-01-20 21:05:54 -0800

    refactored map again

---
+++ my-app/src/app/map.tsx
@@ -0,0 +1,55 @@
+'use client';
+import { RefObject, useEffect, useRef, useState } from "react";
+import {
+    E_SDK_EVENT,
+    getVenueMaker,
+    Mappedin,
+    MapView,
+    showVenue,
+    TGetVenueMakerOptions,
+} from "@mappedin/mappedin-js";
+import "@mappedin/mappedin-js/lib/mappedin.css";
+
+const useVenue = (options: TGetVenueMakerOptions) => {
+    const [venue, setVenue] = useState<Mappedin | undefined>();
+    useEffect(() => {
+        getVenueMaker(options).then(v=> setVenue(v));
+    }, [])
+    return venue;
+}
+
+const useMapView = (venue?: Mappedin, map_ref?: RefObject<HTMLDivElement>) => {
+    const [mapView, setMapView] = useState<MapView>();
+    useEffect(() => {
+        if(venue === undefined || map_ref === undefined || map_ref.current === null) return;
+        showVenue(map_ref.current, venue).then(mapView => {
+            setMapView(mapView)
+        })
+    }, [venue])
+
+    useEffect(() => {
+        if(mapView === undefined) return;
+        mapView.on(E_SDK_EVENT.CLICK, () => {
+            mapView.FloatingLabels.labelAllLocations();
+        })
+        return () => {
+            mapView.FloatingLabels.removeAll();
+        }
+    }, [mapView])
+
+    return mapView;
+}
+
+export default function Map() {
+    const mapViewElement = useRef<HTMLDivElement>(null);
+    const venue = useVenue({
+        mapId: "65ac471fca641a9a1399dc2b",
+        key: "65ac6209ca641a9a1399dc4c",
+        secret: "f85d70b9f1b60514543db48191e581de0827c4f01c3e545e3e2e66ec690ecd14",
+    });
+    const mapView = useMapView(venue, mapViewElement);
+
+    return (
+        <div id="map_element" ref={mapViewElement} className="absolute top-0 max-h-screen"></div>
+    )
+}
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,49 +1,17 @@
-'use client'
-import {useEffect } from "react";
 import NavBar from "~/components/navBar"
 import MainSearch from "~/components/mainSearch"
-
-import {
-  E_SDK_EVENT,
-  getVenueMaker,
-  showVenue,
-  TGetVenueMakerOptions,
-} from "@mappedin/mappedin-js";
-import "@mappedin/mappedin-js/lib/mappedin.css";
-
-const options: TGetVenueMakerOptions = {
-  mapId: "65ac471fca641a9a1399dc2b",
-  key: "65ac6209ca641a9a1399dc4c",
-  secret: "f85d70b9f1b60514543db48191e581de0827c4f01c3e545e3e2e66ec690ecd14",
-};
-
-const initMap = async () =>{
-      const venue = await getVenueMaker(options);
-      await showVenue(document.getElementById("app")!, venue).then((mapView) =>{
-        
-      mapView.on(E_SDK_EVENT.CLICK, ()=>{
-        mapView.FloatingLabels.labelAllLocations();
-      })
-    });
-  }
-import React, { useState } from "react";
+import React from "react";
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("./map"), {
+  ssr: false,
+});
 
 export default function Home() {
-  useEffect(() => {
-    initMap();
-  },[])
-  const [count, setCount] = useState(0);
-
   return (
-<<<<<<< Updated upstream
     <div className="w-screen h-screen">
-=======
-    <div id="app">
-      
->>>>>>> Stashed changes
-      <NavBar/>
-      <div id="app" className="absolute top-0 max-h-screen"></div>
-      <MainSearch></MainSearch>
+      <NavBar />
+      <Map />
+      <MainSearch />
     </div>
   );
 }
