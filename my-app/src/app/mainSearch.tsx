4d53781b9e5c71ac1e3c70b0f6362dd93dfd016c
cameron lee <cameron.lee@smus.ca>
2024-01-21 08:03:35 -0800

    commit to build

--- my-app/src/app/mainSearch.tsx
+++ my-app/src/app/mainSearch.tsx
@@ -102,6 +102,7 @@ interface QueryResult {
   description: string
   shelf_id: number
   cover_url: string
+  
 }
 
 export default function MainSearch({ updateShelfNumber }: { updateShelfNumber: (a: number) => void }) {
